9031f75682e75c5e5e3f6aca2b5f230d5733f2db
Руслан Шевченко <r.schewtschenko@mail.ru>
2023-08-08 06:18:07 +0700

    Java pre-project. Практическая задача 1.1.4

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,47 @@
 package jm.task.core.jdbc.util;
 
-public class Util {
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public final class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+//    // testing the program with another DB
+//    private static final String DB_URL = "jdbc:mysql://localhost:3306/kata_preproject_1";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/test";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "root";
+    private static Util instance;
+
+    private Util() {
+
+    }
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DB_DRIVER).getDeclaredConstructor().newInstance();
+            System.out.println("Connection OK!");
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch(Exception ex) {
+            System.out.println("Connection ERROR!");
+            ex.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    public static Util getInstance() {
+        Util localInstance = instance;
+        if (null == localInstance) {
+            synchronized (Util.class) {
+                localInstance = instance;
+                if (null == localInstance) {
+                    instance = localInstance = new Util();
+                }
+            }
+        }
+        return localInstance;
+    }
 }
